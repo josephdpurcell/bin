4134126b1590f8fc593ae98a672cdfb8767cc892
Joseph D. Purcell <josephdpurcell@gmail.com>
2016-11-13 15:31:37 +0000

    Add drupal sync mailer scripts

---
+++ sync-drupal-7.x-mailer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+SYNC_LOG=/home/joep/logs/sync-drupal-7.x.log
+/home/joep/bin/sync-drupal-7.x.sh 2>&1 > $SYNC_LOG
+
+grep "Everything up-to-date" $SYNC_LOG 2>&1 > /dev/null
+if [ $? -eq 0 ]
+then
+    /bin/cat $SYNC_LOG | /usr/bin/mail -s "CRON: Drupal 7.x Sync NO CHANGE" josephdpurcell@gmail.com
+else
+    /bin/cat $SYNC_LOG | /usr/bin/mail -s "CRON: Drupal 7.x Sync CHANGED" josephdpurcell@gmail.com
+fi
