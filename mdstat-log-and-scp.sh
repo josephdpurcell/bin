b9bb334bcb96c560b56c7a4bacfe1a81bb699f3f
Joseph D. Purcell <josephdpurcell@gmail.com>
2016-11-13 17:07:16 -0500

    Output mdstat log to home logs

--- mdstat-log-and-scp.sh
+++ mdstat-log-and-scp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-/home/joep/bin/mdstat-reporter.sh 2>&1 | tee /var/log/mdstat-custom.log
-scp /var/log/mdstat-custom.log joep@josephdpurcell.com:logs/mdstat-custom.log
-mv /var/log/mdstat-custom.log /var/log/mdstat-custom.log.1
+/home/joep/bin/mdstat-reporter.sh 2>&1 | tee /home/joep/logs/mdstat-custom.log
+scp /home/joep/logs/mdstat-custom.log joep@josephdpurcell.com:logs/mdstat-custom.log
+mv /home/joep/logs/mdstat-custom.log /home/joep/logs/mdstat-custom.log.1
