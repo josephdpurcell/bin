370ea2c6056a08e32e2e6cbed44bab4ee7d2f51c
Joseph D. Purcell <josephdpurcell@gmail.com>
2016-11-13 16:47:30 -0500

    Pipe mdstat errors to log

--- mdstat-log-and-scp.sh
+++ mdstat-log-and-scp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-/root/bin/mdstat-reporter.sh | tee /var/log/mdstat-custom.log
+/home/joep/bin/mdstat-reporter.sh 2>&1 | tee /var/log/mdstat-custom.log
 scp /var/log/mdstat-custom.log joep@josephdpurcell.com:logs/mdstat-custom.log
 mv /var/log/mdstat-custom.log /var/log/mdstat-custom.log.1
