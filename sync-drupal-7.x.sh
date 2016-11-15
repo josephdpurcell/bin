3f3146bddbbe783ec78b4c56fe332bc04a4bfc0d
Joseph D. Purcell <josephdpurcell@gmail.com>
2016-11-15 22:01:27 +0000

    Remove drupal branch specific sync scripts

--- sync-drupal-7.x.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Starting Drupal 7.x sync"
-date
-cd /home/joep/src/github.com/josephdpurcell/drupal-7.x/ 2>&1
-git fetch drupal 7.x 2>&1
-git pull --rebase drupal 7.x 2>&1
-git push -f origin 7.x 2>&1
--- sync-drupal-8.2.x.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Starting Drupal 8.2.x sync"
-date
-cd /home/joep/src/github.com/josephdpurcell/drupal-8.2.x/ 2>&1
-git fetch drupal 8.2.x 2>&1
-git pull --rebase drupal 8.2.x 2>&1
-git push origin -f 8.2.x 2>&1
