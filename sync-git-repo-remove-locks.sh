22ec83dc1b8f099c9ae871fcc488f5e8ab9b8b89
Joseph D. Purcell <josephdpurcell@gmail.com>
2016-11-17 19:40:26 +0000

    Add generic sync git repo remove locks

---
+++ sync-git-repo-remove-locks.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+rm /tmp/sync-drupal-7.x.lock
+rm /tmp/sync-drupal-8.0.x.lock
+rm /tmp/sync-drupal-8.1.x.lock
+rm /tmp/sync-drupal-8.2.x.lock
+rm /tmp/sync-drupal-9.x.lock
+rm /tmp/sync-platform.lock
+rm /tmp/sync-orocommerce.lock
+rm /tmp/sync-magento2.lock
+rm /tmp/sync-symfony.lock
