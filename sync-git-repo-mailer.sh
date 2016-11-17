a8f620c4be13ffd1fa236ae1e90051f7feafff94
Joseph D. Purcell <josephdpurcell@gmail.com>
2016-11-17 19:40:11 +0000

    Add generic sync git repo mailer

---
+++ sync-git-repo-mailer.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+GIT_REPO=$1
+GIT_BRANCH=$2
+GIT_DIR="/home/joep/src/github.com/josephdpurcell/$GIT_REPO/"
+SYNC_LOG="/home/joep/logs/sync-"$GIT_BRANCH".log"
+SYNC_LOCK="/tmp/sync-"$GIT_REPO".lock"
+
+if [ -z $GIT_BRANCH ]
+then
+    echo "Usage: sync-git-repo-mailer.sh [REPO] [BRANCH]"
+    echo "Example: sync-git-repo-mailer.sh drupal-8.1.x 8.1.x"
+    exit 1
+fi
+
+if [ ! -d $GIT_DIR ]
+then
+    echo "Could not find directory $GIT_DIR"
+    exit 1
+fi
+
+/home/joep/bin/sync-git-repo.sh $GIT_REPO $GIT_BRANCH 2>&1 > $SYNC_LOG
+if [ $? -ne 0 ]
+then
+    if [ -f $SYNC_LOCK ]
+    then
+        rm $SYNC_LOCK
+    fi
+    /bin/cat $SYNC_LOG | /usr/bin/mail -s "CRON: $GIT_REPO:$GIT_BRANCH Sync FAILED" josephdpurcell@gmail.com
+    exit 1
+fi
+
+grep "Everything up-to-date" $SYNC_LOG 2>&1 > /dev/null
+if [ $? -eq 0 ]
+then
+    # Only say nothing changed once a day.
+    if [ ! -f $SYNC_LOCK ]
+    then
+        touch $SYNC_LOCK
+        /bin/cat $SYNC_LOG | /usr/bin/mail -s "CRON: $GIT_REPO:$GIT_BRANCH Sync NO CHANGE" josephdpurcell@gmail.com
+    fi
+else
+    if [ -f $SYNC_LOCK ]
+    then
+        rm $SYNC_LOCK
+    fi
+    /bin/cat $SYNC_LOG | /usr/bin/mail -s "CRON: $GIT_REPO:$GIT_BRANCH Sync CHANGED" josephdpurcell@gmail.com
+fi
