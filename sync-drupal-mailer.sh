71dffa7658e21c8baf4844880f8b256bf44b27b1
Joseph D. Purcell <josephdpurcell@gmail.com>
2016-11-17 17:12:45 +0000

    Make drupal-sync-mailer use generic sync script

--- sync-drupal-mailer.sh
+++ sync-drupal-mailer.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
-DRUPAL_BRANCH=$1
-SYNC_LOG="/home/joep/logs/sync-drupal-"$DRUPAL_BRANCH".log"
-SYNC_LOCK="/tmp/sync-drupal-"$DRUPAL_BRANCH".lock"
+GIT_BRANCH=$1
+GIT_REPO="drupal-$GIT_BRANCH"
+SYNC_LOG="/home/joep/logs/sync-drupal-"$GIT_BRANCH".log"
+SYNC_LOCK="/tmp/sync-drupal-"$GIT_BRANCH".lock"
 
-if [ -z $DRUPAL_BRANCH ]
+if [ -z $GIT_BRANCH ]
 then
     echo "Usage: sync-drupal-mailer.sh [BRANCH]"
     echo "Example: sync-drupal-mailer.sh 8.1.x"
     exit 1
 fi
 
-/home/joep/bin/sync-drupal.sh $DRUPAL_BRANCH 2>&1 > $SYNC_LOG
+/home/joep/bin/sync-git-repo.sh $GIT_REPO $GIT_BRANCH 2>&1 > $SYNC_LOG
 if [ $? -ne 0 ]
 then
     if [ -f $SYNC_LOCK ]
     then
         rm $SYNC_LOCK
     fi
-    /bin/cat $SYNC_LOG | /usr/bin/mail -s "CRON: Drupal $DRUPAL_BRANCH Sync FAILED" josephdpurcell@gmail.com
+    /bin/cat $SYNC_LOG | /usr/bin/mail -s "CRON: Drupal $GIT_BRANCH Sync FAILED" josephdpurcell@gmail.com
     exit 1
 fi
 
@@ -28,12 +29,12 @@ then
     if [ ! -f $SYNC_LOCK ]
     then
         touch $SYNC_LOCK
-        /bin/cat $SYNC_LOG | /usr/bin/mail -s "CRON: Drupal $DRUPAL_BRANCH Sync NO CHANGE" josephdpurcell@gmail.com
+        /bin/cat $SYNC_LOG | /usr/bin/mail -s "CRON: Drupal $GIT_BRANCH Sync NO CHANGE" josephdpurcell@gmail.com
     fi
 else
     if [ -f $SYNC_LOCK ]
     then
         rm $SYNC_LOCK
     fi
-    /bin/cat $SYNC_LOG | /usr/bin/mail -s "CRON: Drupal $DRUPAL_BRANCH Sync CHANGED" josephdpurcell@gmail.com
+    /bin/cat $SYNC_LOG | /usr/bin/mail -s "CRON: Drupal $GIT_BRANCH Sync CHANGED" josephdpurcell@gmail.com
 fi
