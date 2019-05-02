c6fc2b0929fb4ac9720e7154dbe83c363f182c9c
Joseph D. Purcell <josephdpurcell@gmail.com>
2019-05-02 11:07:50 +0000

    Backup 4 weeks and day of week

--- joep-server/mysql-backup-weekly.sh
+++ joep-server/mysql-backup-weekly.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Started weekly mysql backup on "$(date)
-WOY=$(date '+%V')
+WOY=$((`date '+%V'` % 4))
 mysqldump --defaults-file=/home/joep/.my.cnf --all-databases | gzip -c > /var/local/backups/mysql/week-$WOY-mysql.sql.gz
 ls -lha /var/local/backups/mysql/week-*
 echo "Completed weekly mysql backup on "$(date)
--- joep-server/mysql-backup.sh
+++ joep-server/mysql-backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Started daily mysql backup on "$(date)
-DOM=$(date '+%d')
-mysqldump --defaults-file=/home/joep/.my.cnf --all-databases > /var/local/backups/mysql/day-$DOM-mysql.sql.gz
+DOW=$(date '+%u')
+mysqldump --defaults-file=/home/joep/.my.cnf --all-databases > /var/local/backups/mysql/day-$DOW-mysql.sql.gz
 ls -lha /var/local/backups/mysql/day-*
 echo "Completed daily mysql backup on "$(date)
