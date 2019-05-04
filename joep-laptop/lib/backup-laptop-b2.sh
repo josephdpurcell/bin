bcd0cb1211fcf006881ed596f99cecfe7bcdc53f
Joseph D. Purcell <josephdpurcell@gmail.com>
2019-05-04 04:53:31 -0500

    Add exclusions to b2 command to avoid recursion and failures

--- joep-laptop/lib/backup-laptop-b2.sh
+++ joep-laptop/lib/backup-laptop-b2.sh
@@ -5,6 +5,11 @@
 # sudo pip3 install --upgrade b2
 # b2 authorize-account "applicationKeyID" "applicationKey"
 #
+B2='/usr/local/bin/b2'
 echo "Started b2 backup on "$(date)
-b2 sync --excludeRegex 'vendor|.cache|VirtualBox VMs|.mozilla' ~/ b2://josephdpurcell-joep-laptop-dbs/home
+# Note: .marks is excluded only because of its symlinks
+# Note: .platform is excluded to avoid symlink recursion
+# Note: .lastpass is excluded to avoid symlinks
+#b2 sync --excludeRegex '^(.config|.local|.marks|vendor|.cache|VirtualBox VMs|.mozilla)' ~/ b2://josephdpurcell-joep-laptop-dbs/home
+$B2 sync --threads 32 --excludeAllSymlinks --excludeDirRegex '^(.*\.platform|.lastpass|.config|Downloads|.local|.marks|vendor|.cache|VirtualBox VMs|.mozilla)' ~/ b2://josephdpurcell-joep-laptop-dbs/home
 echo "Completed b2 backup on "$(date)
