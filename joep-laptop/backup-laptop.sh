301eec36f6e8ed7eabcbfd6cc5d6ab1fdbce5212
Joseph D. Purcell <josephdpurcell@gmail.com>
2019-05-04 04:52:37 -0500

    Add force flag to laptop backup

--- joep-laptop/backup-laptop.sh
+++ joep-laptop/backup-laptop.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 LOCKFILE="$XDG_CACHE_HOME/backup-laptop.lock"
 
-if [ ! -f "$LOCKFILE" ]
+if [[ "$1" == "-f" ]]
+then
+  echo "Force backup, touching lockfile $LOCKFILE"
+  touch $LOCKFILE
+elif [ ! -f "$LOCKFILE" ]
 then
   echo "Lock file not found, creating it at $LOCKFILE"
   touch $LOCKFILE
 # Older than 24 hrs
 elif test "`find "$LOCKFILE" -mmin +1440`"
 then
-  echo "Lock file older than 24 hrs, updating"
+  echo "Lock file older than 24 hrs, updating $LOCKFILE"
   touch $LOCKFILE
 else
-  echo "Lock file is too recent, exiting."
+  echo "Lock file is too recent $LOCKFILE, exiting."
   exit 0
 fi
 
