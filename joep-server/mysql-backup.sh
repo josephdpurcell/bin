ff3617fe01de2fbd2921196222fd8e4963855754
Joseph D. Purcell <josephdpurcell@gmail.com>
2019-08-31 06:26:47 +0000

    Properly gzip daily backups

--- joep-server/mysql-backup.sh
+++ joep-server/mysql-backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Started daily mysql backup on "$(date)
 DOW=$(date '+%u')
-mysqldump --defaults-file=/home/joep/.my.cnf --all-databases > /var/local/backups/mysql/day-$DOW-mysql.sql.gz
+mysqldump --defaults-file=/home/joep/.my.cnf --all-databases | gzip -c > /var/local/backups/mysql/day-$DOW-mysql.sql.gz
 ls -lha /var/local/backups/mysql/day-*
 echo "Completed daily mysql backup on "$(date)
