da150881cb40de19d1221992f9a4b019723326fd
Joseph D. Purcell <josephdpurcell@gmail.com>
2019-08-31 06:25:57 +0000

    Backup www by day of week

--- joep-server/b2-backup.sh
+++ joep-server/b2-backup.sh
@@ -6,10 +6,12 @@
 # b2 authorize-account "applicationKeyID" "applicationKey"
 #
 B2='/usr/local/bin/b2'
+DOW=`date "+%u"`
+
 echo "Started b2 backup on "$(date)
 $B2 sync /var/local/backups/ b2://josephdpurcell-joep-server/backups
 
-$B2 sync /var/www/ b2://josephdpurcell-joep-server/www
+$B2 sync --delete /var/www/ b2://josephdpurcell-joep-server/www/daily-$DOW
 
 echo "Completed b2 backup on "$(date)
 
